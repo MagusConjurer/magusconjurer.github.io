1631a2cf1d99a872a0b76ca8e725de4295e5c312
MagusConjurer <cameronisadavis@gmail.com>
2020-04-24 18:26:07 -0600

    Completed script to dynamically add cards for portfolio. Added dynamic scaling of the portfolio card titles.

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -5,20 +5,23 @@ var gifSearch = $("<div>").addClass("col mb-4");
 var train = $("<div>").addClass("col mb-4");
 var dabbler = $("<div>").addClass("col mb-4");
 
+var personalCards = [hangman, rpgGame, trivia, gifSearch, train];
+var groupCards = [dabbler];
+
 hangman.html("<div class='card portfolio'>" +
     "<img src='assets/images/picsum1021.jpg' class='card-img' alt='https://i.picsum.photos/id/1021/200/200.jpg'>" +
     "<a href='https://magusconjurer.github.io/Word-Guess-Game/' class='portfolio-link'>" +
       "<div class='card-img-overlay'>" +
-        "<h6 class='card-title text-center'>Hangman</h6>" +
+        "<h6 class='card-title text-center portfolioTitles'>Hangman</h6>" +
       "</div>" +
     "</a>" +
   "</div>");
 
 rpgGame.html("<div class='card portfolio'>" +
-  "<img src=assets/images/picsum1054.jpg' class='card-img' alt='https://i.picsum.photos/id/1054/200/200.jpg'>" +
+  "<img src='assets/images/picsum1054.jpg' class='card-img' alt='https://i.picsum.photos/id/1054/200/200.jpg'>" +
     "<a href='https://magusconjurer.github.io/unit-4-game/' class='portfolio-link'>" +
       "<div class='card-img-overlay'>" +
-        "<h6 class='card-title text-center'>RPG Game</h6>" +
+        "<h6 class='card-title text-center portfolioTitles'>RPG Game</h6>" +
       "</div>" +
     "</a>" +
   "</div>");
@@ -27,7 +30,7 @@ trivia.html("<div class='card portfolio'>" +
     "<img src='assets/images/picsum1038.jpg' class='card-img' alt='https://i.picsum.photos/id/1038/200/200.jpg'>" +
     "<a href='https://magusconjurer.github.io/TriviaGame/' class='portfolio-link'>" +
       "<div class='card-img-overlay'>" +
-        "<h6 class='card-title text-center'>Shakespeare Trivia</h6>" +
+        "<h6 class='card-title text-center portfolioTitles'>Shakespeare Trivia</h6>" +
       "</div>" +
     "</a>" +
   "</div>");
@@ -36,7 +39,7 @@ gifSearch.html("<div class='card portfolio'>" +
   "<img src='assets/images/picsum1041.jpg' class='card-img' alt='https://i.picsum.photos/id/1041/200/200.jpg'>" +
     "<a href='https://magusconjurer.github.io/unit-6-ajax/' class='portfolio-link'>" +
       "<div class='card-img-overlay'>" +
-        "<h6 class='card-title text-center'>GIF Search</h6>" +
+        "<h6 class='card-title text-center portfolioTitles'>GIF Search</h6>" +
       "</div>" +
     "</a>" +
   "</div>");
@@ -45,7 +48,7 @@ train.html("<div class='card portfolio'>" +
   "<img src='assets/images/picsum1044.jpg' class='card-img' alt='https://i.picsum.photos/id/1044/200/200.jpg'>" +
     "<a href='https://magusconjurer.github.io/Train-Scheduler/' class='portfolio-link'>" +
       "<div class='card-img-overlay'>" +
-        "<h6 class='card-title text-center'>Train Scheduler</h6>" +
+        "<h6 class='card-title text-center portfolioTitles'>Train Scheduler</h6>" +
       "</div>" +
     "</a>" +
   "</div>");
@@ -54,16 +57,33 @@ dabbler.html("<div class='card portfolio'>" +
   "<img src='assets/images/picsum117.jpg' class='card-img' alt='https://i.picsum.photos/id/117/200/200.jpg'>" +
     "<a href='https://magusconjurer.github.io/dabbler/' class='portfolio-link'>" +
       "<div class='card-img-overlay'>" +
-        "<h6 class='card-title text-center'>Dabbler</h6>" +
+        "<h6 class='card-title text-center portfolioTitles'>Dabbler</h6>" +
       "</div>" +
     "</a>" +
   "</div>");
 
+function loadCards(type){
+  if(type == "personal"){
+    $("#portfolioDeck").empty();
+    for(var i = 0; i < personalCards.length; i++){
+      $("#portfolioDeck").append(personalCards[i]);
+    }
+  } else if (type == "group") {
+    $("#portfolioDeck").empty();
+    for(var i = 0; i < groupCards.length; i++){
+      $("#portfolioDeck").append(groupCards[i]);
+    }
+  }
+};
 
+
+$(document).ready(loadCards("personal"));
 
 
 $(document).on("click", "#personalBtn", function(){
-  $("#portfolioDeck").append(dabbler);
-  
+  loadCards("personal");
+});
 
+$(document).on("click", "#groupBtn", function(){
+  loadCards("group");
 });
