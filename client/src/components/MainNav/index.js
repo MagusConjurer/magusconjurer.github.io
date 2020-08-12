ba7a8bcd81e3e859f64d65de6a96dd40f4b2e146
MagusConjurer <cameronisadavis@gmail.com>
2020-08-11 18:59:18 -0600

    Convert navbar to use react-bootstrap

---
+++ client/src/components/MainNav/index.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+
+class MainNav extends Component {
+  render() {
+    return(
+      <Navbar bg="dark" variant="dark" expand='lg'>
+        <Navbar.Brand href="#!">Cameron Davis</Navbar.Brand>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarMain"
+            aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+        <Navbar.Collapse id="navbarMain">
+          <Nav className="ml-auto">
+            <Nav.Link href="/about" className="ml-auto">About</Nav.Link>
+            <Nav.Link href="/projects" className="ml-auto">Projects</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    );
+  }
+}
+
+export default MainNav;
