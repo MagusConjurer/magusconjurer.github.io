636aaa881f20981d473db9934eb7976d859a437f
MagusConjurer <cameronisadavis@gmail.com>
2020-08-17 20:46:09 -0600

    Add react router and placeholder for projects page

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import MainNav from './components/MainNav';
 import MainFooter from './components/MainFooter';
 import About from './pages/About';
+import Projects from './pages/Projects';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import { faPhone, faEnvelope } from '@fortawesome/free-solid-svg-icons';
@@ -16,7 +18,12 @@ function App() {
   return (
     <div className="container">
       <MainNav />
-      <About />
+      <Router>
+        <Switch>
+          <Route path="/projects" component={Projects} />
+          <Route path="/*" component={About} />
+        </Switch>
+      </Router>
       <MainFooter />
     </div>
   );
