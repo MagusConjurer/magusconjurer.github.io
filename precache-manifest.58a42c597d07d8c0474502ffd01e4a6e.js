13dc7c665991c4ec44ac8df0457c80c93845ca1e
MagusConjurer <cameronisadavis@gmail.com>
2020-09-03 22:05:25 -0600

    Updates

---
+++ precache-manifest.58a42c597d07d8c0474502ffd01e4a6e.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72eea06b0aff4c6ab297aa889dabbeba",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a11bc0e2b26e5676d344",
+    "url": "./static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "6b05047626454d59f9a8",
+    "url": "./static/css/main.7e845234.chunk.css"
+  },
+  {
+    "revision": "a11bc0e2b26e5676d344",
+    "url": "./static/js/2.99dcb3a4.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.99dcb3a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b05047626454d59f9a8",
+    "url": "./static/js/main.60478df2.chunk.js"
+  },
+  {
+    "revision": "d90c5b93796701d5570a",
+    "url": "./static/js/runtime-main.8b0ae790.js"
+  },
+  {
+    "revision": "8e10868327a64c18cf11c7e46cc1de92",
+    "url": "./static/media/LIRI.8e108683.png"
+  },
+  {
+    "revision": "ffd62061cf2824ca71ccd6fc31c49b33",
+    "url": "./static/media/bamazon.ffd62061.png"
+  },
+  {
+    "revision": "6b9086b2fac2527bf8f4bcb79df84fc4",
+    "url": "./static/media/cameron-photo.6b9086b2.jpg"
+  },
+  {
+    "revision": "61caf85b0b2d794cbd6bb71e82ce5648",
+    "url": "./static/media/dabbler.61caf85b.png"
+  },
+  {
+    "revision": "886dc16bef13b22ba0d7cba2886429f4",
+    "url": "./static/media/hopping.886dc16b.png"
+  },
+  {
+    "revision": "6932baf68cdc668173cf71300b08e90a",
+    "url": "./static/media/memory.6932baf6.png"
+  },
+  {
+    "revision": "b81f0463742e29a4f2b88b4ff60d1691",
+    "url": "./static/media/news.b81f0463.png"
+  },
+  {
+    "revision": "66c0e5bcff9faf87ddb5171cabc5cc3b",
+    "url": "./static/media/ptb.66c0e5bc.png"
+  },
+  {
+    "revision": "fec60c48d92dfe3d73a008248e2016ee",
+    "url": "./static/media/rpg.fec60c48.png"
+  },
+  {
+    "revision": "1747d51d95cd20810e18649271d1e857",
+    "url": "./static/media/tripleD.1747d51d.png"
+  }
+]);
